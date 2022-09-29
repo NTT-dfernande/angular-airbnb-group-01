a98fa7d136aa18cc0b42663c1138b2aae1ab3be5
Raquel Beatriz Mansilla Herencia <rmansill@everis.com>
2022-09-29 18:08:28 +0200

    Add component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
